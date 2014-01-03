3c036dd1d26148a9fb3fae0ce57c7c95c2bfa279
bengwinter <ben.g.winter@gmail.com>
2014-01-03 17:24:23 -0500

    added method for multiple csv file parsing and added 2000 data to file

--- american_wealth_parse.rb
+++ american_wealth_parse.rb
@@ -3,43 +3,75 @@ require 'json'
 require 'date'
 require 'pry'
 
+document_array = ["2013_analysis.csv", "2000_analysis.csv"]
+documents_created_array = ["2013_percentile_data.csv", "2000_percentile_data.csv"]
 
 full_array = []
 check_array = []
 i = 0
-CSV.foreach("2013_analysis.csv") do |row|
-  income_entries = []
-  key = row.shift
-  income_averages = []
-  breakdown = row.each_slice(2).to_a
-  breakdown.each do |x|
-    x[0].to_i.times do 
-      income_entries << x[1].to_i
+j = 0
+document_array.each do |doc|
+  CSV.foreach(doc) do |row|
+    income_entries = []
+    key = row.shift
+    income_averages = []
+    breakdown = row.each_slice(2).to_a
+    breakdown.each do |x|
+      x[0].to_i.times do 
+        income_entries << x[1].to_i
+      end
+    end
+    split_value = (income_entries.length/100)
+    percentiles = income_entries.each_slice(split_value).to_a
+    income_averages << key
+    percentiles.each do |y|
+      income_sum = y.inject(:+)
+      num_entries = y.length
+      income_average = (income_sum/num_entries)
+      income_averages << income_average
+    end
+    full_array << income_averages
+    check_array << income_averages.length
+    i += 1
+    puts i
+  end
+
+  CSV.open(documents_created_array[j], "w") do |csv|
+    full_array.each do |x|
+      csv << x
     end
   end
-  split_value = (income_entries.length/100)
-  percentiles = income_entries.each_slice(split_value).to_a
-  income_averages << key
-  percentiles.each do |y|
-    income_sum = y.inject(:+)
-    num_entries = y.length
-    income_average = (income_sum/num_entries)
-    income_averages << income_average
-  end
-  full_array << income_averages
-  check_array << income_averages.length
-  i += 1
-  puts i
+  j += 1
 end
 
-CSV.open("2013_percentile_data.csv", "w") do |csv|
-  full_array.each do |x|
-    csv << x
-  end
-end
+# if loop does not work, just make two of these to execute
+# CSV.foreach("2013_analysis.csv") do |row|
+#   income_entries = []
+#   key = row.shift
+#   income_averages = []
+#   breakdown = row.each_slice(2).to_a
+#   breakdown.each do |x|
+#     x[0].to_i.times do 
+#       income_entries << x[1].to_i
+#     end
+#   end
+#   split_value = (income_entries.length/100)
+#   percentiles = income_entries.each_slice(split_value).to_a
+#   income_averages << key
+#   percentiles.each do |y|
+#     income_sum = y.inject(:+)
+#     num_entries = y.length
+#     income_average = (income_sum/num_entries)
+#     income_averages << income_average
+#   end
+#   full_array << income_averages
+#   check_array << income_averages.length
+#   i += 1
+#   puts i
+# end
 
-# File.open("2013_analysis.csv", 'r').each_line do |line|
-#      binding.pry
-
-#   line.chop.split(',')
+# CSV.open("2013_percentile_data.csv", "w") do |csv|
+#   full_array.each do |x|
+#     csv << x
+#   end
 # end
